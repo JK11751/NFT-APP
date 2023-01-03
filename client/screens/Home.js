18167539190a56480bcdd658a2a242e0864d82b2
Gimo <kgimase@gmail.com>
2023-01-03 15:25:57 +0300

    initial updates

---
+++ client/screens/Home.js
@@ -0,0 +1,11 @@
+import React ,{ useState } from 'react'
+import { View,SafeArea, FlatList, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    
+   <Text>Homw</Text>
+  )
+}
+
+export default Home
